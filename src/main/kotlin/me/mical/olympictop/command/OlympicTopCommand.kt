8af7c7b54dfafe4d72b899b27d9a1d6244d6f7a2
Mical <micalhl@sina.cn>
2021-07-29 22:39:47 +0800

    permission

--- src/main/kotlin/me/mical/olympictop/command/OlympicTopCommand.kt
+++ src/main/kotlin/me/mical/olympictop/command/OlympicTopCommand.kt
@@ -2,6 +2,7 @@ package me.mical.olympictop.command
 
 import me.mical.olympictop.api.OlympicAPI
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command
@@ -9,7 +10,11 @@ import taboolib.common.platform.command
 object OlympicTopCommand {
 
     fun register() {
-        command("olympic") {
+        command(
+            name = "olympic",
+            permission = "OlympicTop.use",
+            permissionMessage = "${ChatColor.RED}I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.",
+        ) {
             literal("open") {
                 dynamic(optional = true) {
                     execute<ProxyCommandSender> { _, _, argument ->
